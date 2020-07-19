ce8f181c522048f9b260452e87b629ee74766309
khandaker mushfiqur rahman <1505086.kmr@ugrad.cse.buet.ac.bd>
2020-07-19 21:57:05 +0600

    one api test written for get_all_words

--- words/tests.py
+++ words/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APITestCase
+from words.views import word_list
+
+
 # Create your tests here.
+class word_list_test(APITestCase):
+
+    def test_see_all_words(self):
+        response = self.client.get('/get_all_words/')
+        self.assertEqual(200, response.status_code)
+
+
+
